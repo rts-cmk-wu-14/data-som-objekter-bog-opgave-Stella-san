899fca59c705fa7bd90f8517a879f23915c46c14
Rikke Gleerup <rikkeglee@gmail.com>
2025-08-27 08:44:32 +0200

    opgave 1 løst

---
+++ script.js
@@ -0,0 +1,26 @@
+let person = {
+    firstname: "Rikke",
+    lastname: "Gleerup",
+    age: "19",
+    hometown: "Rønnede",
+    pet: "Kornsno",
+    petname: "Apollo"
+}
+
+console.log("Dot:", person.firstname);
+console.log("Dot:", person.lastname);
+console.log("Dot:", person.age);
+console.log("Dot:", person.hometown);
+console.log("Dot:", person.pet);
+console.log("Dot:", person.petname);
+
+console.log("Bracket:", person["firstname"]);
+console.log("Bracket:", person["lastname"]);
+console.log("Bracket:", person["age"]);
+console.log("Bracket:", person["hometown"]);
+console.log("Bracket:", person["pet"]);
+console.log("Bracket:", person["petname"]);
+
+
+
+
