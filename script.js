6a16773f17a2b3d286057b8d9a76354cbc0c985c
Rikke Gleerup <rikkeglee@gmail.com>
2025-08-27 09:09:43 +0200

    opgave 2 løst

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
+
+// Opgave 1
+// let person = {
+//     firstname: "Rikke",
+//     lastname: "Gleerup",
+//     age: "19",
+//     hometown: "Rønnede",
+//     pet: "Kornsno",
+//     petname: "Apollo"
+// }
+
+// console.log("Dot:", person.firstname);
+// console.log("Dot:", person.lastname);
+// console.log("Dot:", person.age);
+// console.log("Dot:", person.hometown);
+// console.log("Dot:", person.pet);
+// console.log("Dot:", person.petname);
+
+// console.log("Bracket:", person["firstname"]);
+// console.log("Bracket:", person["lastname"]);
+// console.log("Bracket:", person["age"]);
+// console.log("Bracket:", person["hometown"]);
+// console.log("Bracket:", person["pet"]);
+// console.log("Bracket:", person["petname"]);
+
+
+// Opgave 2
 let person = {
     firstname: "Rikke",
     lastname: "Gleerup",
     age: "19",
     hometown: "Rønnede",
-    pet: "Kornsno",
-    petname: "Apollo"
+    hobbies: ["Tegne", "Spille", "Spejder"],
+    pet: {
+        type: "Kornsno",
+        name: "Apollo"
+    }
 }
 
-console.log("Dot:", person.firstname);
-console.log("Dot:", person.lastname);
-console.log("Dot:", person.age);
-console.log("Dot:", person.hometown);
-console.log("Dot:", person.pet);
-console.log("Dot:", person.petname);
+person.hobbies.forEach(function(hobby){
+    console.log(hobby);
+})
 
-console.log("Bracket:", person["firstname"]);
-console.log("Bracket:", person["lastname"]);
-console.log("Bracket:", person["age"]);
-console.log("Bracket:", person["hometown"]);
-console.log("Bracket:", person["pet"]);
-console.log("Bracket:", person["petname"]);
-
-
+console.log(person.pet.type);
+console.log(person.pet.name);
 
 
